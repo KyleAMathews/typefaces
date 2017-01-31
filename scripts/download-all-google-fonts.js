66ddc89c015b4ad8956116aa56011ad9d466a833
Kyle Mathews <mathews.kyle@gmail.com>
2017-01-31 14:07:54 -0800

    Show progress of downloading google fonts

--- scripts/download-all-google-fonts.js
+++ scripts/download-all-google-fonts.js
@@ -6,7 +6,7 @@ const baseurl = `https://google-webfonts-helper.herokuapp.com/api/fonts/`
 const res = requestSync(`GET`, baseurl)
 const typefaces = JSON.parse(res.getBody(`UTF-8`))
 
-typefaces.forEach((typeface) => {
-  console.log(`downloading ${typeface.id}`)
+typefaces.forEach((typeface, i) => {
+  console.log(`downloading ${typeface.id} ${i}/${typefaces.length}`)
   exec(`node scripts/create-package-from-google-font-id.js ${typeface.id}`)
 })
