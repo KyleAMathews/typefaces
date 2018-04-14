a5262da449a4c2e866afd66ee24fe469dc854e52
Kyle Mathews <mathews.kyle@gmail.com>
2018-04-14 11:47:52 -0700

    Remove do-hyeon as NPM says the package is too similar to others
    If someone really wants this, we could put together a work around

--- scripts/download-all-google-fonts.js
+++ scripts/download-all-google-fonts.js
@@ -8,6 +8,11 @@ const res = requestSync(`GET`, baseurl)
 const typefaces = JSON.parse(res.getBody(`UTF-8`))
 
 async.forEachLimit(typefaces, 10, (typeface, cb) => {
+  // For some reason NPM complains that I can't publish this as it's
+  // too similar to an existing package :shrug:
+  if (typeface.id === `do-hyeon`) {
+    return cb()
+  }
   console.log(
     `downloading ${typeface.id} ${typefaces.indexOf(typeface)}/${
       typefaces.length
