7dcb9869728d77a31d69898943334619d5c2d423
Kyle Mathews <mathews.kyle@gmail.com>
2018-04-21 09:59:41 -0700

    We don't support eot, ttf, and svg any more

--- scripts/templates.js
+++ scripts/templates.js
@@ -51,8 +51,8 @@ The CSS and web font files to easily self-host “<%= typefaceName %>”.
 ## Use
 
 Typefaces assume you’re using webpack to process CSS and files. Each typeface
-package includes all necessary font files (woff2, woff, eot, ttf, svg) and
-a CSS file with font-face declarations pointing at these files.
+package includes all necessary font files (woff2, woff) and a CSS file with
+font-face declarations pointing at these files.
 
 You will need to have webpack setup to load css and font files. Many tools built
 with Webpack will work out of the box with Typefaces such as [Gatsby](https://github.com/gatsbyjs/gatsby)
